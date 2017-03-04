cd6d43d88b95de77f33d90685dc5477ffa2a7f6a
Kyle Banks <banksky1@gmail.com>
2017-03-04 11:44:26 -0500

    Create precommit.mk

---
+++ precommit.mk
@@ -0,0 +1,5 @@
+# Installs a pre-commit Git hook that executes "make sanity" prior to commiting.
+precommit:
+  @echo "make sanity" > .git/hooks/pre-commit
+  @chmod +x .git/hooks/pre-commit
+.PHONY: precommit
