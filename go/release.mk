08530d4fffe32df0ce05460249a06e85f7dbd114
KyleBanks <kyle@nthgensoftware.com>
2017-03-06 06:59:15 -0500

    Fixing spacing on go/release.mk

--- go/release.mk
+++ go/release.mk
@@ -1,10 +1,10 @@
 ifndef RELEASE_PKG
-        RELEASE_PKG = .
+	RELEASE_PKG = .
 endif
 
 # Creates release binaries for each supported platform/architecture.
 release: | sanity
-        @echo "Making release of '$(RELEASE_PKG)'"
-        @gox -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm windows/386 windows/amd64" \
+	@echo "Making release of '$(RELEASE_PKG)'"
+	@gox -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm windows/386 windows/amd64" \
                                 -output "bin/{{.Dir}}_{{.OS}}_{{.Arch}}" $(RELEASE_PKG)
 .PHONY: release
