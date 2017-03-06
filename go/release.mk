9a151efaabee009d04e3713929ed76905b0e3ebb
Kyle Banks <banksky1@gmail.com>
2017-03-06 06:56:32 -0500

    Update release makefile to allow a custom package

--- go/release.mk
+++ go/release.mk
@@ -1,5 +1,12 @@
+include github.com/KyleBanks/make/go/sanity
+
+ifndef RELEASE_PKG
+        RELEASE_PKG = .
+endif
+
 # Creates release binaries for each supported platform/architecture.
 release: | sanity
-	@gox -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm windows/386 windows/amd64" \
-		-output "bin/{{.Dir}}_{{.OS}}_{{.Arch}}" .
+        @echo "Making release of '$(RELEASE_PKG)'"
+        @gox -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm windows/386 windows/amd64" \
+                                -output "bin/{{.Dir}}_{{.OS}}_{{.Arch}}" $(RELEASE_PKG)
 .PHONY: release
