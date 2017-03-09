bb541d438140720f0bc327c7f42dd47d6711ad3d
KyleBanks <kyle@nthgensoftware.com>
2017-03-08 20:44:21 -0500

    Ability to specify version when releasing go binaries

--- go/release.mk
+++ go/release.mk
@@ -4,7 +4,7 @@ endif
 
 # Creates release binaries for each supported platform/architecture.
 release: | sanity
-	@echo "Making release of '$(RELEASE_PKG)'"
+	@echo "Making release of $(RELEASE_PKG) v$(VERSION)"
 	@gox -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 linux/arm windows/386 windows/amd64" \
-                                -output "bin/{{.Dir}}_{{.OS}}_{{.Arch}}" $(RELEASE_PKG)
+                                -output "bin/{{.Dir}}_$(VERSION)_{{.OS}}_{{.Arch}}" $(RELEASE_PKG)
 .PHONY: release
